01b5313bd8a653995407f7b53f42dbde83fe49b2
Terry <82017869+2218084076@users.noreply.github.com>
2022-04-26 19:28:38 +0800

    update

--- src/get_data/main.py
+++ src/get_data/main.py
@@ -1,19 +1,21 @@
 # coding=utf-8
-import asyncio
 import logging
 import random
 import time
 from pathlib import Path
-from playwright.sync_api import sync_playwright
+
 import cv2
 import numpy as np
 import pandas as pd
 import pyautogui
+from playwright.sync_api import sync_playwright
 
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+
+result_json = []
 
 
 def read_csv(file_path: Path) -> list:
@@ -45,10 +47,11 @@ def main(file_path: str, year: str, start_month: str, end_month: str) -> None:
     page = browser.new_page()
     # Go to http://43.248.49.97/
     page.goto("http://43.248.49.97/")
+    page.wait_for_timeout(200)
     title = page.title()
-    pyautogui.hotkey('win', 'up')
     logging.debug('page title: %s' % title)
     page.wait_for_timeout(1000)
+
     for key in read_csv(Path(file_path)):
         logging.debug('find %s' % key)
         page.frame_locator("iframe").nth(1).locator("select[name=\"year\"]").select_option(year)
@@ -89,85 +92,63 @@ def main(file_path: str, year: str, start_month: str, end_month: str) -> None:
         page.wait_for_timeout(300)
         # Click text=确定
         page.frame_locator("iframe").nth(1).locator("text=确定").click()
-        page.wait_for_timeout(2000)
-        msg_element = page.locator('#msg')
-        logging.debug('msg_element: %s' % msg_element)
-        # page.evaluate('document.getElementById("msg").value = "1"')
-        text = page.evaluate('document.getElementById("msg").innerText')
-        logging.debug('text_msg: %s' % text)
-        # Close page
-        page.wait_for_timeout(300)
-        # Click text=确定
+        page.wait_for_timeout(200)
         page.frame_locator("iframe").nth(1).frame_locator("iframe[name=\"layui-layer-iframe2\"]").locator(
             "text=确定").click()
-        page.wait_for_timeout(200)
-        page.frame_locator("iframe").nth(1).locator("text=返回设置").click()
-        page.wait_for_timeout(500)
-        page.close()
+        page.frame_locator('iframe').nth(1).frame_locator('iframe').locator('#msg').evaluate(
+            'document.getElementById("msg").value="1"')
+        msg = page.frame_locator('iframe').nth(1).frame_locator('iframe').locator('#msg').evaluate(
+            'document.getElementById("msg").value')
+        logging.debug('msg_value: %s' % msg)
+        # click 确定
+        page.frame_locator('iframe').nth(1).frame_locator('iframe').locator('#doSearch').click()
+        page.wait_for_timeout(100)
+
+        page_size = page.frame_locator("iframe").nth(1).locator('.c-666').nth(0).inner_text()
+        page_num = int(page_size.split('共查询到')[1].split('条数据')[0])
+        if page_num > 10:
+            page.frame_locator("iframe").nth(1).locator('#pageSize').select_option('200')
+            page.frame_locator("iframe").nth(1).locator('#pageSize').click()
+            logging.debug('page size: %s' % page_size)
+        else:
+            logging.debug('page size: %s' % page_size)
+        if page_num == 0:
+            logging.debug('page=0')
+        else:
+
+            for p in range(page_num):
+                info = page.frame_locator("iframe").nth(1).locator('#table').evaluate('''
+    get_info = function() {
+      var a, i, json, l, n, r_l, _i, _j, _len, _len1;
+      a = document.getElementsByClassName("text-c  mt-10")[%s];
+      l = a.getElementsByTagName("td");
+      r_l = [];
+      for (_i = 0, _len = l.length; _i < _len; _i++) {
+        i = l[_i];
+        r_l.push(i.innerText);
+        for (_j = 0, _len1 = r_l.length; _j < _len1; _j++) {
+          n = r_l[_j];
+          json = {
+            'id': r_l[0],
+            'product_name': r_l[1],
+            'first_q': r_l[2],
+            'first_n': r_l[3],
+            'second_q': r_l[4],
+            'second_n': r_l[5],
+            'rmb': r_l[6]
+          };
+        }
+      }
+      return json;
+    };
+                ''' % p)
+                result_json.append(info)
+                logging.debug("get info: %s" % info)
+
+    page.wait_for_timeout(2000)
+    page.close()
     logging.debug('playwright stop')
     browser.close()
-
-
-def mouse_action(x: int):
-    """
-    mouse movement
-    :param x:
-    """
-    pyautogui.moveTo(919, 675)
-    pyautogui.click(919, 675)
-    time.sleep(random.uniform(1, 2))
-    pyautogui.dragTo(970 + x, 675, duration=random.uniform(0, 1))
-    time.sleep(random.uniform(1, 2))
-
-
-def mark_edge(image_path: str) -> int:
-    """
-    Mark image edges
-    :param image_path:
-    """
-    e_max = 0
-    max_contours = []
-    p = cv2.imread(image_path)
-    hsv = cv2.cvtColor(p, cv2.COLOR_BGR2HSV)
-    lower_hsv = np.array([0, 0, 255])  # 提取颜色的低值
-    high_hsv = np.array([180, 30, 255])
-    mask = cv2.inRange(hsv, lowerb=lower_hsv, upperb=high_hsv)
-    ret, binary = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(p, contours, -1, (0, 0, 255), 3)
-    for i in contours:
-        if len(i) > e_max:
-            e_max = len(i)
-            max_contours = i.tolist()
-    # print(e_max, max_list)
-    return max_contours[len(max_contours) - 1][0][0]
-
-
-def crop_image(image_path: str):
-    """
-    crop_image and save
-    :param page image_path:
-    """
-    logging.debug('-crop_image-')
-    img = cv2.imread(image_path)
-    roi = (727, 268, 310, 154)
-    x, y, w, h = (727, 268, 310, 154)
-    if roi != (0, 0, 0, 0):
-        crop = img[y:y + h, x:x + w]
-        cv2.imwrite('1.jpg', crop)
-        logging.debug('Saved!')
-
-
-def download(page):
-    """
-    click to download
-    """
-    page.frame_locator("iframe").nth(1).locator("a:has-text(\"导出数据\")").click()
-    # Click text=确定
-    with page.expect_download() as download_info:
-        page.frame_locator("iframe").nth(1).locator("text=确定").click()
-    download_ = download_info.value
-    print(download_)
 
 
 # 830 712 1010712
@@ -176,4 +157,5 @@ def download(page):
 # id_list = read_csv('商品参数导出.csv')
 # print(id_list)
 # for d in id_list:
-main('商品参数导出.csv', '2021', '1', '3')
+main('商品参数导出.csv', '2021', '1', '12')
+print(result_json)
